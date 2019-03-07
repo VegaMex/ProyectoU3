c2792eda6eab8ac9eac50fc20fb41c74c1bd0090
Óscar Vega <s16120245@alumnos.itsur.edu.mx>
2019-03-06 22:02:19 -0600

    Agregados proyectos
    Se han agregado los proyectos Frontend y Backend.

---
+++ Backend/Listas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class Listas
+    {
+
+
+
+        public List<string> nombresH = new List<string>();
+        public List<string> nombresM = new List<string>();
+        public List<string> apellidos = new List<string>();
+
+        public void inicializarNombresH()
+        {
+            nombresH = File.ReadAllLines("").ToList();
+        }
+
+        public void inicializarNombresM()
+        {
+
+        }
+    }
+}
